17a9e9488bca8f8bf4e823af794c12bd31a2661e
roflsunriz <roflsunriz@yahoo.co.jp>
2026-08-21 23:21:37 +0900

    chore: TypeScript/Vite/ESLint/Prettier/監査スクリプトの導入

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist/", "node_modules/", "test/", "*.png"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+    },
+  }
+);
